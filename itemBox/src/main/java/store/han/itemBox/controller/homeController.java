9607b5e4855b8d2602d4433f759825cb98ef24bd
DESKTOP-MG3B588\jun <aaddd2@naver.com>
2023-10-17 18:42:09 +0900

    메인페이지 테만

--- itemBox/src/main/java/store/han/itemBox/controller/homeController.java
+++ itemBox/src/main/java/store/han/itemBox/controller/homeController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class homeController {
     @GetMapping("/")
     public String home(){
-
-        return "home";
+        return "index";
     }
+
+
 }
